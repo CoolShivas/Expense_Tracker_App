8478583954bd130219a1ba5de38883caf6dbab8f
ketansoni694@gmail.com <ketansoni694@gmail.com>
2024-02-25 01:04:23 +0530

    Component_ExpenseDetails

---
+++ Expense_Tracker_App/src/components/ExpenseDetails.jsx
@@ -0,0 +1,8 @@
+const ExpenseDetails = (props) => {
+    return <>
+        <h2> {props.nameXYZ} </h2>
+        {props.amountXYZ}
+    </>
+}
+
+export default ExpenseDetails;
--- Expense_Tracker_App/src/components/ExpenseItem.jsx
+++ Expense_Tracker_App/src/components/ExpenseItem.jsx
@@ -1,19 +1,37 @@
 import styles from "./ExpenseItem.module.css";
 import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
+
 
 const ExpenseItem = (props) => {
 
     return (
         <div className={styles.expense_item}>
+
             {/* <div> {props.dateABC.toISOString()} </div> */}
-            <ExpenseDate dateXYZ={props.dateABC}></ExpenseDate>
+
+            <ExpenseDate
+                dateXYZ={props.dateABC}
+            ></ExpenseDate>
+
             <div
                 className={styles.expense_item__description}>
-                <h2> {props.nameABC} </h2>
+
+                {/* <h2> {props.nameABC} </h2> */}
+
+                <ExpenseDetails
+                    nameXYZ={props.nameABC}></ExpenseDetails>
             </div>
+
             <div
                 className={styles.expense_item__price}>
-                ₹ {props.amountABC}
+
+                {/* ₹ {props.amountABC} */}
+
+                <ExpenseDetails
+                    amountXYZ={props.amountABC}
+                ></ExpenseDetails>
+
             </div>
         </div>
     )
