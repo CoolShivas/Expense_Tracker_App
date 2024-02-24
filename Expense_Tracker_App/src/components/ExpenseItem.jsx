dcc8c64687533e3d9063ecbdf35d7e4461d6401f
ketansoni694@gmail.com <ketansoni694@gmail.com>
2024-02-24 16:20:16 +0530

    Creation of components

---
+++ Expense_Tracker_App/src/components/ExpenseItem.jsx
@@ -0,0 +1,12 @@
+import ExpenseItems from "./ExpenseItems";
+
+const ExpenseItem = () => {
+    return (
+        <div>
+            <h2> ExpenseItems. </h2>
+            <ExpenseItems></ExpenseItems>
+        </div>
+    )
+}
+
+export default ExpenseItem;
