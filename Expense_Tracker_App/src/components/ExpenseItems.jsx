2107afc23a84db9b1cc96b33e0dc6fca1271ed80
ketansoni694@gmail.com <ketansoni694@gmail.com>
2024-02-24 23:38:17 +0530

    Use_Of_Map

--- Expense_Tracker_App/src/components/ExpenseItems.jsx
+++ Expense_Tracker_App/src/components/ExpenseItems.jsx
@@ -1,30 +1,16 @@
 import ExpenseItem from "./ExpenseItem";
 
-const ExpenseItems = (props) => {
+const ExpenseItems = ({ expensesABC }) => {
     return (
         <div>
-            <ExpenseItem
-                nameABC={props.expensesABC[0].title}
-                amountABC={props.expensesABC[0].amount}
-                dateABC={props.expensesABC[0].date}
-            ></ExpenseItem>
-            <ExpenseItem
-                nameABC={props.expensesABC[1].title}
-                amountABC={props.expensesABC[1].amount}
-                dateABC={props.expensesABC[1].date}
-            ></ExpenseItem>
-            <ExpenseItem
-                nameABC={props.expensesABC[2].title}
-                amountABC={props.expensesABC[2].amount}
-                dateABC={props.expensesABC[2].date}
-            ></ExpenseItem>
-            <ExpenseItem
-                nameABC={props.expensesABC[3].title}
-                amountABC={props.expensesABC[3].amount}
-                dateABC={props.expensesABC[3].date}
-            ></ExpenseItem>
-
-
+            {expensesABC.map((arr) => {
+                return <ExpenseItem
+                    nameABC={arr.title}
+                    amountABC={arr.amount}
+                    dateABC={arr.date}
+                    key={arr.title}
+                ></ExpenseItem>
+            })}
         </div>
     )
 }
