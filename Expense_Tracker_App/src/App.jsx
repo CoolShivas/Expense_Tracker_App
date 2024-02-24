e20f95eda0511d2611588d70ea8c1405f0900863
ketansoni694@gmail.com <ketansoni694@gmail.com>
2024-02-24 22:17:35 +0530

    Passing_Data_Via_Props

--- Expense_Tracker_App/src/App.jsx
+++ Expense_Tracker_App/src/App.jsx
@@ -1,10 +1,57 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+
+  const expenses = [
+    {
+      id: "e1",
+      title: "Auto Insurance",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "Bike Insurance",
+      amount: 194.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 2594.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e4",
+      title: "Dumper Insurance",
+      amount: 1294.12,
+      date: new Date(2020, 7, 14),
+    },
+  ];
+
   return (
     <div>
       <h1> Let's get started. </h1>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        nameABC={expenses[0].title}
+        amountABC={expenses[0].amount}
+        dateABC={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        nameABC={expenses[1].title}
+        amountABC={expenses[1].amount}
+        dateABC={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        nameABC={expenses[2].title}
+        amountABC={expenses[2].amount}
+        dateABC={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        nameABC={expenses[3].title}
+        amountABC={expenses[3].amount}
+        dateABC={expenses[3].date}
+      ></ExpenseItem>
     </div>
   )
 }
--- Expense_Tracker_App/src/components/ExpenseItem.jsx
+++ Expense_Tracker_App/src/components/ExpenseItem.jsx
@@ -1,19 +1,20 @@
 import styles from "./ExpenseItem.module.css";
 
-const ExpenseItem = () => {
-    const expenseDate = new Date(2024, 2, 12);
-    const expenseTitle = "Car Insurance";
-    const expenseAmount = 294;
+const ExpenseItem = (props) => {
+    // const expenseDate = new Date(2024, 2, 12);
+    // const expenseTitle = "Car Insurance";
+    // const expenseAmount = 294;
     return (
         <div className={styles.expense_item}>
-            <div> {expenseDate.toISOString()} </div>
+            <div> {props.dateABC.toISOString()}
+            </div>
             <div
                 className={styles.expense_item__description}>
-                <h2> {expenseTitle} </h2>
+                <h2> {props.nameABC} </h2>
             </div>
             <div
                 className={styles.expense_item__price}>
-                ₹ {expenseAmount}
+                ₹ {props.amountABC}
             </div>
         </div>
     )
