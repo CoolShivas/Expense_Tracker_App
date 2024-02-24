92aa99756ac1934fbb308e973c809ed7e8fb86ec
ketansoni694@gmail.com <ketansoni694@gmail.com>
2024-02-24 21:03:15 +0530

    Second-Commit

--- Expense_Tracker_App/src/components/ExpenseItem.jsx
+++ Expense_Tracker_App/src/components/ExpenseItem.jsx
@@ -1,10 +1,20 @@
-import ExpenseItems from "./ExpenseItems";
+import styles from "./ExpenseItem.module.css";
 
 const ExpenseItem = () => {
+    const expenseDate = new Date(2024, 2, 12);
+    const expenseTitle = "Car Insurance";
+    const expenseAmount = 294;
     return (
-        <div>
-            <h2> ExpenseItems. </h2>
-            <ExpenseItems></ExpenseItems>
+        <div className={styles.expense_item}>
+            <div> {expenseDate.toISOString()} </div>
+            <div
+                className={styles.expense_item__description}>
+                <h2> {expenseTitle} </h2>
+            </div>
+            <div
+                className={styles.expense_item__price}>
+                ₹ {expenseAmount}
+            </div>
         </div>
     )
 }
